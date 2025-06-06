3de77231adbdd714954581ee03ac1d7db117c841
Will Weinzettl <will.weinzettl@number41media.com>
2025-06-06 11:48:29 -0700

    CJG-1709 Minor update to SQL script to add DateUpdated

--- CJG.Infrastructure.EF/Migrations/v02.18.01/PostDeploy/Seed/01_UpdateCourseOutline.sql
+++ CJG.Infrastructure.EF/Migrations/v02.18.01/PostDeploy/Seed/01_UpdateCourseOutline.sql
@@ -1,8 +1,9 @@
 ﻿PRINT 'Start of Course Outline update'
 
-Update [dbo].[TrainingProviderTypes]
-Set CourseOutline=1
-where Caption like '%B.C. Public Post-Secondary Institution%'
-and IsActive=1
+UPDATE [dbo].[TrainingProviderTypes]
+SET CourseOutline = 1,
+    DateUpdated = GETUTCDATE()
+WHERE Caption LIKE '%B.C. Public Post-Secondary Institution%'
+AND IsActive = 1
 
 PRINT 'End of Course Outline update'
