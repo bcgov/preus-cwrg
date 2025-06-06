6b6b76d47c560cdc2a5066e8fb7b118742970de3
Sunandita Sahu <sunanditasahu@gmail.com>
2025-06-06 11:31:51 -0700

    CJG-1709: Updated CourseOutline value to 1 in database so that File upload option can be enabled in Skills Training view.

---
+++ CJG.Infrastructure.EF/Migrations/v02.18.01/PostDeploy/Seed/01_UpdateCourseOutline.sql
@@ -0,0 +1,8 @@
+﻿PRINT 'Start of Course Outline update'
+
+Update [dbo].[TrainingProviderTypes]
+Set CourseOutline=1
+where Caption like '%B.C. Public Post-Secondary Institution%'
+and IsActive=1
+
+PRINT 'End of Course Outline update'
